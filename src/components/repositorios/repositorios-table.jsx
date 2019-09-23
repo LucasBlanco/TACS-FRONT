882c484fb81ace4127f6ff9e9bcf774205f16041
LucasBlanco <lucas_manuel_blanco@hotmail.com>
2019-09-22 22:19:20 -0300

    Creo pantalla basica de repositorios

---
+++ src/components/repositorios/repositorios-table.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Table } from 'antd'
+
+export const RepositoriosTable = ({ repositorios }) => {
+
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+        },
+    ];
+    return (
+        <Table dataSource={repositorios} columns={columns} />
+    )
+}
--- src/components/repositorios/repositorios.jsx
+++ src/components/repositorios/repositorios.jsx
@@ -1,13 +1,105 @@
 import React, { Component } from 'react'
+import { Row, Card, Typography, Col, Form, Input, Button, InputNumber } from 'antd';
+import { RepositoriosTable } from './repositorios-table';
 
+const { Title } = Typography
 
 class Repositorios extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            repositorios: [
+                {
+                    key: '1',
+                    name: 'Mike',
+                    age: 32,
+                    address: '10 Downing Street',
+                },
+                {
+                    key: '2',
+                    name: 'John',
+                    age: 42,
+                    address: '10 Downing Street',
+                },
+            ]
+        }
     }
+
     render() {
-        return (<h1>Repositorios</h1>);
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 8 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 16 },
+            },
+        };
+        return (
+            <Row>
+                <Row style={{ paddingBottom: 20 }}>
+                    <Card>
+                        <Row>
+                            <Title level={2}>Filtros</Title>
+                        </Row>
+                        <Row gutter={24} type="flex" justify="start">
+                            <Form  {...formItemLayout} onSubmit={this.handleSubmit}>
+                                <Col lg={8}>
+                                    <Form.Item label="Issues">
+                                        <InputNumber
+                                            placeholder="1"
+                                            min="0"
+                                        />
+
+                                    </Form.Item>
+                                </Col>
+                                <Col lg={8}>
+                                    <Form.Item label="Commits">
+                                        <InputNumber
+                                            placeholder="1"
+                                            min="0"
+                                        />
+                                    </Form.Item>
+                                </Col>
+                                <Col lg={8}>
+                                    <Form.Item label="Language">
+                                        <Input
+                                            placeholder="Java"
+                                        />
+                                    </Form.Item>
+                                </Col>
+                                <Col lg={8}>
+                                    <Form.Item label="Score">
+                                        <InputNumber
+                                            placeholder="999"
+                                            min="0"
+                                        />
+                                    </Form.Item>
+                                </Col>
+                                <Col lg={8}>
+                                    <Form.Item label="Subscribers">
+                                        <InputNumber
+                                            placeholder="1"
+                                            min="0"
+                                        />
+                                    </Form.Item>
+                                </Col>
+                            </Form>
+                        </Row>
+                        <Row type="flex" justify="end">
+                            <Button type="primary">Buscar</Button>
+                        </Row>
+                    </Card>
+                </Row>
+                <Row>
+                    <RepositoriosTable
+                        repositorios={this.state.repositorios}
+                    >
+                    </RepositoriosTable>
+                </Row>
+            </Row>
+        );
     }
 }
 
