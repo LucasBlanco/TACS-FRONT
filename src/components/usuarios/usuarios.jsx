d5b9b75fb90efcdff473c7ef86de1ff3c8dbb62c
Lu <lucia_roldan@outlook.com>
2019-09-26 18:52:08 -0300

    Agrega pagina de usuarios

--- src/components/usuarios/usuarios.jsx
+++ src/components/usuarios/usuarios.jsx
@@ -1,11 +1,61 @@
 import React, { Component } from 'react'
+import { Table } from 'antd';
+import { getUsers } from '../../services/user-service';
 
 export default class Usuarios extends Component {
+
+
+    constructor() {
+        super()
+        this.state = {
+            users: [],
+            selectedIds: []
+        }
+        getUsers().then(_users => {
+            console.log(_users)
+            const users = _users.map(user => {
+                return { ...user, admin: user.admin.toString(), key: user.id }
+            })
+            this.setState({ users })
+        })
+    }
+
+
+
     render() {
+
+
+        const rowSelection = {
+            selectedRowKeys: this.state.selectedIds,
+            onChange: (selectedRowKeys, selectedRows) => {
+                if (selectedRowKeys.length > 2) {
+                    const [key1, key2, ...keys] = selectedRowKeys
+                    this.setState({ selectedIds: [key1, key2] })
+                } else {
+                    this.setState({ selectedIds: selectedRowKeys })
+                }
+                console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+            }
+        };
+
+        const columns = [
+            {
+                title: 'User name',
+                dataIndex: 'username',
+                key: 'username',
+            },
+            {
+                title: 'Admin',
+                dataIndex: 'admin',
+                key: 'admin',
+            }
+
+        ];
         return (
-            <div>
-                Usuarios
-            </div>
+            <React.Fragment>
+                <h2>Users</h2>
+                <Table dataSource={this.state.users} columns={columns} rowSelection={rowSelection} />
+            </React.Fragment>
         )
     }
 }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,10 +1,16 @@
 
-import { post } from 'axios'
+import { post, get } from 'axios'
 import serverRoute from './server-route'
+import { User } from '../models/usuario'
 
 export const createUser = ({ username, password }) => {
     return post(serverRoute + "/users", {
         username,
         password
     })
+}
+
+export const getUsers = () => {
+    return get(serverRoute + "/users")
+        .then(response => response.data.map(user => new User(user)))
 }
